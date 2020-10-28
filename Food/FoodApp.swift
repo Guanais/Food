5bbbda8f6d87a4bb25a44b03796bb3f03cbe7af2
Kaio <kaioaguanais@gmail.com>
2020-10-28 15:36:53 -0700

    Initial Commit

---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by Kaio Guanais on 2020-10-28.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
